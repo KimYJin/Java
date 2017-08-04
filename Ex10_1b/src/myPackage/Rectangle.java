03d58095d25b07df725717fd4a95b9253ac9c77e
KimYoonJin <95yjkim95@gmail.com>
2017-08-04 23:58:07 +0900

    study Interface with shape project

---
+++ Ex10_1b/src/myPackage/Rectangle.java
@@ -0,0 +1,26 @@
+package myPackage;
+
+public class Rectangle implements Shape {
+    private double height, width;
+
+    public Rectangle(double height, double width) {
+        this.height = height; this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return height * width;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Height: %.2f, Width: %.2f\n", height, width);
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (!(otherObject != null && otherObject instanceof Rectangle)) return false;
+        Rectangle r = (Rectangle)otherObject;
+        return this.height == r.height && this.width == r.width;
+    }
+}
---
+++ Ex10_1b/src/myPackage/RightTriangle.java
@@ -0,0 +1,26 @@
+package myPackage;
+
+public class RightTriangle implements Shape {
+    private double height, width;
+
+    public RightTriangle(double height, double width) {
+        this.height = height; this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return height * width / 2.0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Height: %.2f, Width: %.2f\n", height, width);
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (!(otherObject != null && otherObject instanceof RightTriangle)) return false;
+        RightTriangle t = (RightTriangle)otherObject;
+        return this.height == t.height && this.width == t.width;
+    }
+}
